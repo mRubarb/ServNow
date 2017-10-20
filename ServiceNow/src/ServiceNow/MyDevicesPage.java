e1de69e7261da534d22dbcbc0c6718ab6a154075
Bob.Lichtenfels <Bob.Lichtenfels@BLICHTENFEL6440.corp.tangoe.com>
2017-10-20 09:42:26 -0400

    b4 merge

--- ServiceNow/src/ServiceNow/MyDevicesPage.java
+++ ServiceNow/src/ServiceNow/MyDevicesPage.java
@@ -2,6 +2,7 @@ package ServiceNow;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import HelperObjects.DeviceInfoActions;
 
@@ -110,5 +111,30 @@ public class MyDevicesPage extends BaseClass
 		new Select(driver.findElement(By.xpath(chooseActionsPullDownServices))).selectByVisibleText(DeviceInfoActions.portNumber);
 	}			
 	
-	
+	// find first device that has no service and select the deactivate action on it.
+	public static void SelectFirstDeviceWithNoService()
+	{
+		int cntr;
+		boolean foundDeviceWithNoService = false;
+
+		// get the number of available devices on the page.
+		int numDevices =  driver.findElements(By.xpath("//button[text()='View Device']")).size();
+		
+		// go through each device until one with no service is found.
+		for(cntr= 1; cntr <= numDevices; cntr++)
+		{
+			if(driver.findElement(By.xpath("(//button[text()='View Device'])[" + cntr + "]/../preceding-sibling::div[1]")).getText().equals("")) // device has no service number shown.
+			{
+				foundDeviceWithNoService = true;
+				break;
+			}
+		}
+		
+		if(!foundDeviceWithNoService) // throw error if a devices with no service was not found.
+		{ 
+			Assert.fail("Failed to find a device with no service in MyDevicesPage.SelectFirstDeviceWithNoService method.");
+		}
+
+		new Select(driver.findElement(By.xpath("(//select)[" + cntr +"]"))).selectByVisibleText(DeviceInfoActions.deactivate); // select the deactivate action.
+	}
 }
---
+++ ServiceNow/src/TestSuite1/SFD113063_UnfriendlyErrorMessage.java
@@ -0,0 +1,54 @@
+package TestSuite1;
+
+import javax.swing.JOptionPane;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import ActionsBaseClasses.CommonTestSteps;
+import HelperObjects.DeviceInfoActions;
+import ServiceNow.BaseClass;
+import ServiceNow.MyDevicesPage;
+import ServiceNow.TangoeMobilityOrderRequestsPage;
+import ActionClasses.DeactivateService;
+
+public class SFD113063_UnfriendlyErrorMessage extends BaseClass 
+{
+
+	@BeforeClass
+	public static void setUp() throws Exception
+	{
+		//userLoginMode = UserLoginMode.Admin; // setup login for admin user.
+		//setUpDriver(DriverSetup.ServiceNow);
+	}
+
+	@Test
+	public static void SFD113063_UnfriendlyErrorMessageTest()throws Exception
+	{
+		CommonTestSteps.LoginLimitedUser();
+		
+		// got to the devices page through the home page. 
+		CommonTestSteps.GoToDevicesPage();
+		MyDevicesPage.WaitForPageToLoad();
+		MyDevicesPage.SelectFirstDeviceWithNoService();
+
+		Pause("");
+	}
+	
+	@AfterClass
+	public static void closeDriver() throws Exception
+	{
+		System.out.println("Close Browser.");
+		//if(orderDetailsObjectExpected != null)
+		//{
+		//	orderDetailsObjectExpected.Show();			
+		//}
+	    JOptionPane.showMessageDialog(frame, "Select OK to stop the webdriver and browser.");
+		driver.close();
+		driver.quit();
+	}	
+}
