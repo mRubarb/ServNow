c8f79f02d72216bd0803d2d583126ed87ac0a3f8
Bob.Lichtenfels <Bob.Lichtenfels@BLICHTENFEL6440.corp.tangoe.com>
2018-08-01 12:33:47 -0400

    Fixed TC0040UpgradeServiceApprove. Added XML for calling test suite test cases. Added .bat file, xml file, and testNg jar file so the .bat fi;e can run the tests in XML file.

---
+++ ServiceNow/BatchTestSN.bat
@@ -0,0 +1,20 @@
+java -cp "bin;Libs/lib_3_4/*"  org.testng.TestNG TestNgTestXML.xml
+
+pause
+
+
+
+rem NOTE!!!!!!!!!!!!!!!!!!!!!!!!!!!!! - read this line
+rem Below command works for me. Provided that all required jars including testng jar kept inside lib.
+rem java -cp "lib/*" org.testng.TestNG testng.xml
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+rem java -cp bin;lib_312/* org.testng.TestNG src/MyTest.xml
+
+rem C:\Users\Bob Lichtewfels\.m2\repository\org\testng\testng\6.11
+
+rem java -cp bin;lib_312/*   "C:\Users\Bob Lichtewfels\.m2\repository\org\testng\testng\6.11\testng-6.11.jar"  org.testng.TestNG src/MyTest.xml
+rem java -cp bin;lib_312/*   "C:\Users\Bob Lichtewfels\.m2\repository\org\testng\testng\6.11\testng-6.11.jar"  org.testng.TestNG src/MyTest.xml
