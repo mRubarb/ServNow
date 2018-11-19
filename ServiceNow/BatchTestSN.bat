dd26b3294947e88b43743796aa7fa35a470b6724
Bob.Lichtenfels <Bob.Lichtenfels@blichtenfelse6440b.corp.tangoe.com>
2018-11-19 18:32:46 -0500

    11/19/18

--- ServiceNow/BatchTestSN.bat
+++ ServiceNow/BatchTestSN.bat
@@ -9,14 +9,8 @@ rem Below command works for me. Provided that all required jars including testng
 rem java -cp "lib/*" org.testng.TestNG testng.xml
 rem !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-
-
-
 rem java -cp bin;lib_312/* org.testng.TestNG src/MyTest.xml
-
 rem C:\Users\Bob Lichtewfels\.m2\repository\org\testng\testng\6.11
-
 rem java -cp bin;lib_312/*   "C:\Users\Bob Lichtewfels\.m2\repository\org\testng\testng\6.11\testng-6.11.jar"  org.testng.TestNG src/MyTest.xml
 rem java -cp bin;lib_312/*   "C:\Users\Bob Lichtewfels\.m2\repository\org\testng\testng\6.11\testng-6.11.jar"  org.testng.TestNG src/MyTest.xml
 
-pause
---
+++ ServiceNow/DosCopy.bat
@@ -0,0 +1,3 @@
+copy test-output\index.html C:\Users\bob.lichtenfels\.jenkins\workspace\mobproc-api\results-Token
+
+pause
