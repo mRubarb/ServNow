2c5fe2ccccb0b606dd54c3aa2dea078036d8cb09
blichPosts <lichtenf@hotmail.com>
2019-03-12 09:18:32 -0400

    try

--- ServiceNow/src/ServiceNow/OrderSubmittedPage.java
+++ ServiceNow/src/ServiceNow/OrderSubmittedPage.java
@@ -610,7 +610,7 @@ public class OrderSubmittedPage extends BaseClass
 			
 			Assert.assertEquals(driver.findElement(By.xpath("//*[@id='panel-approvals']/div/div[2]/div/table/tbody/tr[1]/td[2]/span/div[1]")).getText(), approverAdmin, errorMessage);
 			Assert.assertEquals(driver.findElement(By.xpath("//*[@id='panel-approvals']/div/div[2]/div/table/tbody/tr[2]/td[2]/span/div[1]")).getText(), userApproverShorterName, errorMessage);
-			// below 2/21/19
+			// below 2/21/19 - changed 3/12/19
 			//Assert.assertEquals(driver.findElement(By.xpath("//*[@id='panel-approvals']/div/div[2]/div/table/tbody/tr[2]/td[2]/span/div[1]")).getText(), userApproverShorterNameFrank, errorMessage);			
 		}
 		
